a24af4abae3fbd2ce579746f28023ddf5ea63320
Mahdi Hazrati <mahdihazratimailbox@gmail.com>
2023-09-13 22:03:17 +0430

    config route

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {BrowserRouter, Routes, Route} from "react-router-dom"
-import App from './App/App';
-import "./Theme/Index.css"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import SingleBook from "./Pages/SingleBook/SingleBook";
+import BookDetails from "./Components/BookDetails/BookDetails";
+import BookList from "./Components/BookList/BookList";
+import "./Theme/Index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App/>} />
-    </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
+    <React.StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />}>
+                  <Route path="/single" element={<SingleBook />} />
+                  <Route path="/book" element={<BookList />} />
+                  <Route path="/book/:id" element={<BookDetails />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    </React.StrictMode>
 );
