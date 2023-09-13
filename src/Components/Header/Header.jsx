04774219ec1139e386fd17a35b60200d169fc6ba
Mahdi Hazrati <mahdihazratimailbox@gmail.com>
2023-09-13 22:34:57 +0430

    Inital Header and add reset css

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import Navbar from "../Navbar/Navbar"
+import SearchForm from "../SearchForm/SearchForm"
+import "./Header.css"
+
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="header-container">
+      <header className="header">
+        <Navbar />
+        <div className="content">
+          <h2 className="title">Find Book</h2>
+          <p className="description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam hic, eaque explicabo dolore fugiat dolorum odio dolorem aspernatur neque similique?</p>
+          <SearchForm />
+        </div>
+      </header>
+    </div>
   )
 }
 
